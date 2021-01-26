d3b4d871d3e4caae10ad5d12cec3ceaafb112508
yuhanj2 <jyhan043@gmail.com>
2021-01-26 12:29:59 +0800

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,132 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-app>
+
+      <v-app-bar app>
+        <v-spacer/>
+        <v-app-bar-title>
+          <div id="institutionName">{{ institutionName }}</div>
+        </v-app-bar-title>
+        <v-spacer/>
+      </v-app-bar>
+
+      <v-navigation-drawer
+          app
+      >
+
+        <v-sheet
+            color="grey lighten-4"
+            class="pa-4"
+        >
+          <v-avatar
+              class="mb-4"
+              color="primary"
+              size="64"
+          >
+            <span class="white--text headline">{{ username ? username.charAt(0) : '' }}</span>
+          </v-avatar>
+          <div id="username">{{ username }}</div>
+          <div id="userPhoneNumber">{{ userPhoneNumber }}</div>
+        </v-sheet>
+
+        <v-divider></v-divider>
+
+        <v-list>
+          <v-list-item
+              v-for="[icon, link, content] in links"
+              :key="icon"
+              @click="changeCurrContent(content)"
+              :class="currContent === content ? 'navbarLinksSelected' : '' "
+              link
+          >
+            <v-list-item-icon>
+              <v-icon
+                  large
+                  color="purple darken-2"
+              >
+                {{ icon }}
+              </v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ link }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+
+      </v-navigation-drawer>
+
+      <!-- Sizes your content based upon application components -->
+      <v-main style="padding-top: 0">
+
+        <!-- Provides the application the proper gutter -->
+        <v-container fluid>
+<!--          <img alt="Vue logo" src="./assets/logo.png">-->
+<!--          <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+          <!-- If using vue-router -->
+<!--          <router-view></router-view>-->
+          <div v-if="currContent == 0"> 待办事项 </div>
+          <Calendar v-if="currContent == 1"/>
+          <div v-if="currContent == 2"> 文件输出 </div>
+
+        </v-container>
+      </v-main>
+
+      <v-footer app>
+        <!-- -->
+      </v-footer>
+    </v-app>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Calendar from "@/components/Calendar";
 
 export default {
   name: 'App',
+  data: () => ({}),
   components: {
-    HelloWorld
-  }
+    Calendar,
+  },
+  computed: {
+    institutionName() {
+      return this.$store.state.institutionName;
+    },
+    username() {
+      return this.$store.state.username;
+    },
+    links() {
+      return this.$store.state.links;
+    },
+    userPhoneNumber() {
+      return this.$store.state.userPhoneNumber;
+    },
+    currContent() {
+      return this.$store.state.currContent;
+    }
+  },
+  methods: {
+    changeCurrContent(content) {
+      this.$store.commit('changeCurrContent', content);
+    },
+  },
 }
 </script>
 
 <style>
+#username {
+  font-size: 30px;
+}
+
+.navbarLinksSelected {
+  background-color: #6da8e9;
+}
+
+#institutionName {
+  font-size: 30px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/main.js
+++ src/main.js
@@ -1,8 +1,44 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuetify from 'vuetify' // vuetify UI component library
+import 'vuetify/dist/vuetify.min.css' // vuetify css
+import Vuex from 'vuex' // vuex state management
 
 Vue.config.productionTip = false
+Vue.use(Vuetify);
+Vue.use(Vuex);
+
+// vuetify setup
+const vuetifyOpts = {}
+const vuetify = new Vuetify(vuetifyOpts);
+
+// vuex store setup
+const store = new Vuex.Store({
+  state: {
+    // count: 0,
+    currContent: 0,
+    institutionName: "XX美容院",
+    username: "员工 A",
+    userPhoneNumber: "3193336006",
+    cards: ['Today', 'Yesterday'],
+    links: [
+      ['mdi-message', '待办事项', 0],
+      ['mdi-calendar', '日程', 1],
+      ['mdi-file', '文件输出', 2],
+    ],
+  },
+  mutations: {
+    // increment (state) {
+    //   state.count++
+    // }
+    changeCurrContent(state, content){
+      state.currContent = content;
+    }
+  }
+})
 
 new Vue({
+  vuetify, // register vuetify to the entire app
+  store, // register vuex store to the entire app (same as just store in ES6 object property shorthand notation)
   render: h => h(App),
 }).$mount('#app')
